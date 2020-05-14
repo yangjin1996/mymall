9c45669fdd6d5c9bcaaa1c26917cd7625871f9c5
yangjin <2367149000@qq.com>
2020-05-14 23:17:58 +0800

    '禁用格式检测'

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,10 @@ module.exports = {
     devServer: {
         host:'localhost',
         port:8089,
-        // proxy: {
-        //     '/api': {
-        //     target: 'http://www.api.com/index.php/',
-        //     changeOrigin: true,
-        //     // pathRewrite: {
-        //     //     '^/api': '/api'
-        //     // }
-        //     }
-        // }
-    }
+        overlay: {
+            warnings: false,
+            errors: false
+        },
+    },
+    lintOnSave: false
   }
